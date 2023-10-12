871798824bda6ba2e872485db9040a7cba84ec59
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2023-10-12 16:01:25 +0530

    Asg3Ca.java

--- Asg3Ca.java
+++ Asg3Ca.java
@@ -1,3 +1,19 @@
+/*
+ a) Create an interface Department containing attributes deptName and deptHead. It also
+has abstract methods for printing the attributes. Create a class hostel containing
+hostelName, hostelLocation and numberOfRooms. The class contains method printing
+the attributes. Then write Student class extending the Hostel class and implementing
+the Department interface. This class contains attributes studentName, regNo,
+electiveSubject and avgMarks. Write suitable printData method for this class. Also,
+implement the abstract methods of the Department interface. Write a driver class to
+test the Student class. The program should be menu driven containing the options:
+    i. Admit new student
+    ii. Migrate a student
+    iii. Display details of a student
+For the third option, a search is to be made on the basis of the entered registration
+Number.
+ */
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
