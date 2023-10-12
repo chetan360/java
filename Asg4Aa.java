d8a952ab8dc67cf64e947a5adb4e8002ec5a5e4f
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2023-10-12 16:02:08 +0530

    Asg4Aa.java

--- Asg4Aa.java
+++ Asg4Aa.java
@@ -1,3 +1,11 @@
+/*
+ a) Define a class patient (patient_name, patient_age,
+patient_oxy_level,patient_HRCT_report). Create an object of patient. Handle
+appropriate exception while patient oxygen level less than 95% and HRCT scan report
+greater than 10, then throw user defined Exception Patient is Covid Positive(+) and
+Need to Hospitalized otherwise display its information.
+ */
+
 import java.util.Scanner;
 
 class Patient {
