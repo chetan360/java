694741193e46868aed960e37e4027dfcbbf59e09
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2023-10-12 15:57:10 +0530

    Asg2Ba.java

--- Asg2Ba.java
+++ Asg2Ba.java
@@ -1,3 +1,13 @@
+/*
+a) Write a Java program to create a Package SY which has a class SYMarks
+(members – ComputerTotal, MathsTotal, and ElectronicsTotal). Create another
+package TY which has a class TYMarks (members – Theory, Practicals). Create n
+objects of Student class (having rollNumber, name, SYMarks and TYMarks). Add
+the marks of SY and TY computer subjects and calculate the Grade (‘A’ for >= 70,
+‘B’ for >= 60 ‘C’ for >= 50 , Pass Class for > =40 else ‘FAIL’) and display the result
+of the student in proper format.
+ */
+
 import java.util.Scanner;
 
 import SY.SYMarks;
