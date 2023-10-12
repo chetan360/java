62f2e9401edce96189004ac2fbbac1da24ca8207
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2023-10-12 15:58:44 +0530

    Asg3Aa.java

--- Asg3Aa.java
+++ Asg3Aa.java
@@ -1,3 +1,17 @@
+/*
+a) Write a program for multilevel inheritance such that country is inherited from
+continent. State is inherited from country. Display the place, state, country
+and continent.
+        Continent
+            |   
+           \|/
+         Country
+            |   
+           \|/
+          State
+
+*/
+
 class Continent {
     String continentName;
     Continent(String continentName) {
@@ -23,20 +37,23 @@ class Country extends Continent {
 
 class State extends Country {
     String stateName;
+    String place;
     
-    State(String continentName, String countryName, String stateName) {
+    State(String continentName, String countryName, String stateName, String place) {
         super(continentName, countryName);
         this.stateName = stateName;
+        this.place = place;
     }
 
     public void printState() {
         System.out.println("State = " + stateName);
+        System.out.println("Place = " + place);
     }
 }
 
 class Asg3Aa {
     public static void main(String args[]) {
-        State Maharashtra = new State("Asia" , "India", "Maharashtra");
+        State Maharashtra = new State("Asia" , "India", "Maharashtra", "Dabhadi");
         
         Maharashtra.printContinent();
         Maharashtra.printCountry();
