9ab7c67e41d767a8f91723d36a0e606fd9181416
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2023-10-12 15:50:10 +0530

    Update Asg1Bb.java

--- Asg1Bb.java
+++ Asg1Bb.java
@@ -1,3 +1,12 @@
+/* 
+b)Define a class MyNumber having one private int data member. Write a default
+constructor to initialize it to 0 and another constructor to initialize it to a value
+(Use this). Write methods isNegative, isPositive, isZero, isOdd, isEven. Create an
+object in main. Use command line arguments to pass a value to the object
+(Hint : convert string argument to integer) and perform the above tests. Provide
+javadoc comments for all constructors and methods and generate the html help file
+*/
+
 class MyNumber {
     //private int data member
     private int n;
