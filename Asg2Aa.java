e01138399c02d14e635466aa2b142d8e9e0d146c
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2023-10-12 15:53:51 +0530

    Asg2Aa.java

--- Asg2Aa.java
+++ Asg2Aa.java
@@ -1,3 +1,11 @@
+/*
+a) Create an employee class(id,name,deptname,salary). Define a default and
+parameterized constructor. Use ‘this’ keyword to initialize instance variables.
+Keep a count of objects created. Create objects using parameterized constructor
+and display the object count after each object is created.(Use static member and
+method). Also display the contents of each object.
+ */
+
 class Employee {
     int id, salary;
     String name, deptname;
