adc2f49984ba14a9c206fac51d233b198976c183
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2023-10-12 15:52:52 +0530

    Asg1Cc.java

--- Asg1Cc.java
+++ Asg1Cc.java
@@ -1,3 +1,14 @@
+/*
+ c) Write a program to display the 1 to 15 tables.
+( 1 * 1 = 1     2 * 1 = 2     …….   15 * 1 = 15
+  1 * 2 = 2     2 * 2 = 4           15 * 2 = 30
+  1 * 3 = 3     2 * 3 = 6     …     15 * 3 = 45
+
+                              …..
+
+  1 * 10 = 10   2 * 10 = 20   …     15 * 10 = 150)  
+ */
+
 public class Asg1Cc {
     public static void main(String[] args) {
         for(int i=1; i<=15; i++) {
